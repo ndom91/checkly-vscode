152316d196a3d57eb87277827ff3954bdb88cc3c
ndom91 <yo@ndo.dev>
2022-08-17 16:34:12 +0200

    fix: cleanup statusBar entry

--- src/extension.ts
+++ src/extension.ts
@@ -2,34 +2,24 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode'
 
-let myStatusBarItem: vscode.StatusBarItem
+let checklyStatusBarItem: vscode.StatusBarItem
 
 export function activate({ subscriptions }: vscode.ExtensionContext) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  console.log('Congratulations, your extension "checkly-code" is now active!')
-
-  // register a command that is invoked when the status bar
-  // item is selected
-  const myCommandId = 'checkly-code.run'
+  const runCommandId = 'checkly-code.run'
   subscriptions.push(
-    vscode.commands.registerCommand(myCommandId, () => {
-      const n = getNumberOfSelectedLines(vscode.window.activeTextEditor)
-      vscode.window.showInformationMessage(
-        `Yeah, ${n} line(s) selected... Keep going!`
-      )
+    vscode.commands.registerCommand(runCommandId, () => {
+      // vscode.workspace.fs.readDirectory('**/*', '**/*.ts', '**/*.tsx')
+      vscode.window.showInformationMessage(`Running current file...`)
     })
   )
 
-  // create a new status bar item that we can now manage
-  myStatusBarItem = vscode.window.createStatusBarItem(
+  checklyStatusBarItem = vscode.window.createStatusBarItem(
     'checklyCode',
     vscode.StatusBarAlignment.Right,
     Number.MAX_SAFE_INTEGER
   )
-  myStatusBarItem.command = myCommandId
-  myStatusBarItem.show()
-  subscriptions.push(myStatusBarItem)
+  checklyStatusBarItem.command = runCommandId
+  subscriptions.push(checklyStatusBarItem)
 
   // register some listener that make sure the status bar
   // item always up-to-date
@@ -45,26 +35,8 @@ export function activate({ subscriptions }: vscode.ExtensionContext) {
 }
 
 function updateStatusBarItem(): void {
-  const n = getNumberOfSelectedLines(vscode.window.activeTextEditor)
-  if (n > 0) {
-    myStatusBarItem.text = `[🦝] $(megaphone) ${n} line(s) selected`
-    myStatusBarItem.show()
-  } else {
-    myStatusBarItem.hide()
-  }
-}
-
-function getNumberOfSelectedLines(
-  editor: vscode.TextEditor | undefined
-): number {
-  let lines = 0
-  if (editor) {
-    lines = editor.selections.reduce(
-      (prev, curr) => prev + (curr.end.line - curr.start.line),
-      0
-    )
-  }
-  return lines
+  checklyStatusBarItem.text = `🦝 $(raccoon) Run Current File`
+  checklyStatusBarItem.show()
 }
 
 // this method is called when your extension is deactivated
