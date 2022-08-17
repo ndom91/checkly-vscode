df0a787a72862bbaf12ff161c7142cfb863b0a9e
ndom91 <yo@ndo.dev>
2022-08-17 16:06:28 +0200

    feat: initial repo setup

--- src/extension.ts
+++ src/extension.ts
@@ -2,24 +2,69 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "checkly-code" is now active!');
+let myStatusBarItem: vscode.StatusBarItem;
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('checkly-code.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from checkly-code!');
-	});
+export function activate({ subscriptions }: vscode.ExtensionContext) {
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log('Congratulations, your extension "checkly-code" is now active!');
 
-	context.subscriptions.push(disposable);
+  // register a command that is invoked when the status bar
+  // item is selected
+  const myCommandId = 'checkly-code.run';
+  subscriptions.push(
+    vscode.commands.registerCommand(myCommandId, () => {
+      const n = getNumberOfSelectedLines(vscode.window.activeTextEditor);
+      vscode.window.showInformationMessage(
+        `Yeah, ${n} line(s) selected... Keep going!`
+      );
+    })
+  );
+
+  // create a new status bar item that we can now manage
+  myStatusBarItem = vscode.window.createStatusBarItem(
+    'checkly-code',
+    vscode.StatusBarAlignment.Right,
+    9999
+  );
+  myStatusBarItem.command = myCommandId;
+  myStatusBarItem.show();
+  subscriptions.push(myStatusBarItem);
+
+  // register some listener that make sure the status bar
+  // item always up-to-date
+  subscriptions.push(
+    vscode.window.onDidChangeActiveTextEditor(updateStatusBarItem)
+  );
+  subscriptions.push(
+    vscode.window.onDidChangeTextEditorSelection(updateStatusBarItem)
+  );
+
+  // update status bar item once at start
+  updateStatusBarItem();
+}
+
+function updateStatusBarItem(): void {
+  const n = getNumberOfSelectedLines(vscode.window.activeTextEditor);
+  if (n > 0) {
+    myStatusBarItem.text = `[🦝] $(megaphone) ${n} line(s) selected`;
+    myStatusBarItem.show();
+  } else {
+    myStatusBarItem.hide();
+  }
+}
+
+function getNumberOfSelectedLines(
+  editor: vscode.TextEditor | undefined
+): number {
+  let lines = 0;
+  if (editor) {
+    lines = editor.selections.reduce(
+      (prev, curr) => prev + (curr.end.line - curr.start.line),
+      0
+    );
+  }
+  return lines;
 }
 
 // this method is called when your extension is deactivated
